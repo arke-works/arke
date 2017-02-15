573e60dd105d9d66e564307f548b1c281aaf665e
Aleksandr Tihomirov <hunter@zeta.pm>
2017-02-15 22:10:51 +0200

    Fix travis conf, again, again, again, again

--- .travis/always.sh
+++ .travis/always.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+
+set -e
+echo "" > coverage.txt
+
+for d in $(go list ./... | grep -v vendor); do
+    go test -race -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
+done
 bash <(curl -s https://codecov.io/bash)
